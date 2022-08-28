336c11f91d2d2b9b017d57d84036ba7025bdccbd
Louise Thomsen <loui718d@stud.kea.dk>
2022-08-28 19:43:18 +0200

    farver cus

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors:{
+      primary:"2D4263",
+      secondary:"C84B31",
+      accent:"191919",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
