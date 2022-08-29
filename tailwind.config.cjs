d88b5a7f2836206df8dfb4bb91936d4726c2e1aa
Louise Thomsen <loui718d@stud.kea.dk>
2022-08-29 10:00:14 +0200

    billede

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    colors:{
-      primary:"2D4263",
-      secondary:"C84B31",
-      accent:"191919",
+    extend: {
+      colors: {
+        primary: "2D4263",
+        secondary: "C84B31",
+        accent: "191919",
+      },
     },
-    extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
